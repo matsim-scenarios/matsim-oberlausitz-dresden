67eaf9b016f7181673cf31661c80043245ae9db6
paulh <heinrich@vsp.tu-berlin.de>
2025-03-03 10:28:29 +0100

    Add reference trips to dashboardx

---
+++ src/main/java/org/matsim/dashboard/OberlausitzDresdenDashboardProvider.java
@@ -0,0 +1,35 @@
+package org.matsim.dashboard;
+
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.application.prepare.network.CreateAvroNetwork;
+import org.matsim.core.config.Config;
+import org.matsim.run.OberlausitzDresdenScenario;
+import org.matsim.simwrapper.Dashboard;
+import org.matsim.simwrapper.DashboardProvider;
+import org.matsim.simwrapper.SimWrapper;
+import org.matsim.simwrapper.dashboard.TrafficCountsDashboard;
+import org.matsim.simwrapper.dashboard.TripDashboard;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Provider for default dashboards in the scenario.
+ * Declared in META-INF/services
+ */
+public class OberlausitzDresdenDashboardProvider implements DashboardProvider {
+
+	@Override
+	public List<Dashboard> getDashboards(Config config, SimWrapper simWrapper) {
+
+		TripDashboard trips = new TripDashboard("mode_share_ref.csv", "mode_share_per_dist_ref.csv", "mode_users_ref.csv")
+			.setAnalysisArgs("--dist-groups", "0,1000,2000,5000,10000,20000");
+
+		TrafficCountsDashboard counts = new TrafficCountsDashboard()
+			.withModes(TransportMode.car, Set.of(TransportMode.car))
+			.withModes(OberlausitzDresdenScenario.FREIGHT, Set.of(OberlausitzDresdenScenario.FREIGHT));
+
+		return List.of(trips, counts);
+	}
+
+}
